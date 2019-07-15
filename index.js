afbf4a7eaffae59cb2d6ce3478b6c601478669bc
LaTonya Johnson <latonya.johnson0921@gmail.com>
2019-07-15 20:46:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
-  return 'Happy holidays, you!'
+  return 'Happy Independence Day, you!'
 }
 
 function holidayCountdown(holiday, days){
